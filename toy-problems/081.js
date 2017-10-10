c491898c17f36e802944c6c6626046a04874dd28
Roger Sperberg <rsperberg@gmail.com>
2017-10-10 11:54:52 -0400

    Add alternate approaches

--- toy-problems/081.js
+++ toy-problems/081.js
@@ -3,3 +3,14 @@ function addToFrontOfNew(arr, element) {
   arr2.unshift(element);
   return arr2;
 }
+
+// Yusaku Kasahara
+function addToFrontOfNew(arr, element) {
+  return [element].concat(arr);
+}
+
+// Samir Habbu
+function addToFrontOfNew(arr, element) {
+ var newArray = [];
+  return newArray.concat(element, arr);
+}
