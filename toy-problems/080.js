9240f4d774edfee61594d168ec7c6e0885c48baf
Roger Sperberg <rsperberg@gmail.com>
2017-10-10 11:46:55 -0400

    Add standard concat() syntax for multiple arrays

--- toy-problems/080.js
+++ toy-problems/080.js
@@ -2,3 +2,7 @@ function joinThreeArrays(arr1, arr2, arr3) {
   return arr4 = [...arr1, ...arr2, ...arr3];
   // return arr1.concat(arr2).concat(arr3);
 }
+
+// function joinThreeArrays(arr1, arr2, arr3) {
+//   return arr1.concat(arr2, arr3);
+// }
