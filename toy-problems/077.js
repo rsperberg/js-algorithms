14f9568b2b077ac495eaa8fb7a2c0cbbc51d3954
Roger Sperberg <rsperberg@gmail.com>
2017-10-14 13:56:57 -0400

    Add alternate approaches

--- toy-problems/077.js
+++ toy-problems/077.js
@@ -5,3 +5,44 @@ function removeNumberValues(obj) {
     }
   }
 }
+
+// Yusaku Kasahara
+function removeNumberValues(obj) {
+  for (var key in obj) {
+    typeof obj[key] === 'number' ? delete obj[key] : obj;
+  }
+}
+
+// Alexander Mire
+function removeNumberValues(obj) {
+  for(var x in obj){
+    isNaN(obj[x]) ? obj[x] : delete obj[x]
+  }
+}
+
+// Alex Balliet
+function removeNumberValues(obj) {
+  for (var prop in obj) {
+    if (!isNaN(obj[prop])) {
+      delete obj[prop];
+    }
+  }
+}
+
+// Jen Diamond
+function removeNumberValues(obj) {
+  var keys = Object.keys(obj);
+  keys.forEach(function(key){
+    if ( typeof obj[key] === 'number' ){
+      delete obj[key];
+    }
+  });
+  return obj;
+}
+
+// Steve Huy Cao
+function removeNumberValues(obj) {
+  for (var key in obj) {
+    typeof obj[key] === 'number' && delete obj[key];
+  }
+}
