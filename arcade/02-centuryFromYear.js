38a92f8d598874a1f96e58cc3b6a2df07528f86f
Roger Sperberg <rsperberg@gmail.com>
2017-10-16 15:14:07 -0400

    Add info on bitwise OR

--- arcade/02-centuryFromYear.js
+++ arcade/02-centuryFromYear.js
@@ -49,12 +49,25 @@ function centuryFromYear(y) {
   d = y/100 | 0;
   return y%100 == 0 ? d : d+1;
 }
-// A single pipe is a bit-wise OR.
-// Performs the OR operation on each pair of bits. a OR b yields 1 if either a or b is 1.
-// JavaScript truncates any non-integer numbers in bitwise operations, so console.log(0.5 | 0) is computed as 0|0, which is 0.
-// Yahel
-// https://stackoverflow.com/a/6194959/324984
-// Since bitwise operations only make sense on integers, 0.5 is truncated.
-// 0 | x is x, for any x
-// SLaks
-// https://stackoverflow.com/a/6194960/324984
+/*
+See: https://stackoverflow.com/questions/6194950/what-does-the-single-pipe-do-in-javascript
+
+A single pipe is a bit-wise OR.
+Performs the OR operation on each pair of bits. a OR b yields 1 if either a or b is 1.
+JavaScript truncates any non-integer numbers in bitwise operations, so console.log(0.5 | 0) is computed as 0|0, which is 0.
+Yahel
+https://stackoverflow.com/a/6194959/324984
+
+Since bitwise operations only make sense on integers, 0.5 is truncated.
+0 | x is x, for any x
+SLaks
+https://stackoverflow.com/a/6194960/324984
+
+Works for Boolean as well. JS interprets true as 1, false as 0; so
+  alert(true | false) // yields 1;
+  alert(true | true) // yields 1;
+  alert(false | true) // yields 1;
+  alert(false | false) // yields 0
+gordon
+https://stackoverflow.com/a/6195019/324984
+*/
