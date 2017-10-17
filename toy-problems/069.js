7a0d70c6999b5987672df20d53474ba2eb956fb6
Roger Sperberg <rsperberg@gmail.com>
2017-10-17 18:23:10 -0400

    Add alternates

--- toy-problems/069.js
+++ toy-problems/069.js
@@ -8,3 +8,49 @@ function extend(obj1, obj2) {
 	obj1[keys2[i]] = obj2[keys2[i]]
   }
 }
+
+//  Eric Chi
+function extend(obj1, obj2) {
+  var obj2Array = Object.getOwnPropertyNames(obj2);
+  obj2Array.forEach(function (elem) {
+    if (!obj1[elem]) {
+      obj1[elem] = obj2[elem];
+    }
+  })
+}
+
+// John Jacob
+function extend(obj1, obj2) {
+  for (var key in obj2) {
+    if (obj1[key] === undefined) {
+      obj1[key] = obj2[key];
+    }
+  }
+}
+
+// Michael Diodoro
+function extend(obj1, obj2) {
+  for (var key in obj2) {
+    if (!obj1[key]) {
+      obj1[key] = obj2[key];
+    }
+  }
+}
+
+// Vu Chau
+function extend(obj1, obj2) {
+  // Loop through all keys in obj2
+  for(var i in obj2) {
+    // If key isn't found in obj1, add it
+    if(!(i in obj1)) {
+      obj1[i] = obj2[i];
+    }
+  }
+}
+
+// Yusaku Kasahara
+function extend(obj1, obj2) {
+  for (var key in obj2) {
+    obj1[key] ? obj1 : obj1[key] = obj2[key];
+  }
+}
