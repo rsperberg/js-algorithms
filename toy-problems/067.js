4164629645a6676ef72b98662a82d11c492a63a3
Roger Sperberg <rsperberg@gmail.com>
2017-10-20 10:58:30 -0400

    Add alternative

--- toy-problems/067.js
+++ toy-problems/067.js
@@ -14,3 +14,12 @@ function isEitherEvenOrAreBoth7(num1, num2) {
 function isEitherEvenOrAreBoth7(num1, num2) {
   return (num1 % 2 === 0) || (num2 % 2 === 0) || (num1 ===7 && num2 === 7);
 }
+
+// Yusaku Kasahara
+function isEitherEvenOrAreBoth7(num1, num2) {
+  return num1 + num2 === 14 ? true : isEven(num1) ? true : isEven(num2) ? true : false;
+}
+
+function isEven(num) {
+  return num % 2 === 0;
+}
