c3efa50d4ec334e7d24fbe59dea1ab48c5327290
Roger Sperberg <rsperberg@gmail.com>
2017-10-19 20:28:37 -0400

    Initial commit with alternates

---
+++ toy-problems/066.js
@@ -0,0 +1,24 @@
+function or(expression1, expression2) {
+   return (expression1 ? true : (expression2 ? true : false ));
+}
+
+//  Michael Diodoro
+function or(expression1, expression2) {
+  return expression1 ? expression1 : expression2;
+}
+
+// John Jacob
+function or(expression1, expression2) {
+  return !(!expression1 && !expression2);
+}
+
+// Alexander Mire
+function or(expression1, expression2) {
+if(expression1){
+  return true
+}
+if(expression2){
+  return true
+}
+return false
+}
