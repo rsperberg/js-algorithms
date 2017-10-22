53287353a8df36b1291b075c73e2de637ae77b11
Roger Sperberg <rsperberg@gmail.com>
2017-10-22 17:58:25 -0400

    Initial solution

---
+++ arcade/09-allLongestStrings.js
@@ -0,0 +1,40 @@
+/*
+Given an array of strings, return another array containing all of its longest strings.
+
+Example
+
+For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
+
+Input/Output
+[time limit] 4000ms (js)
+[input] array.string inputArray
+
+A non-empty array.
+
+Guaranteed constraints:
+1 ≤ inputArray.length ≤ 10
+1 ≤ inputArray[i].length ≤ 10
+
+[output] array.string
+
+Array of the longest strings, stored in the same order as in the inputArray.
+*/
+function allLongestStrings(inputArray) {
+// generate an array containing the lengths of all the strings in the array
+// use Math.max to find the longest string length
+// loop through this array
+    // if this string matches max length, use index to grab the string from inputArray
+    // push these strings to new array and return it
+    // short-circuit if array contains only one string
+    let resultsArray = [];
+    if (inputArray.length === 1) return inputArray.slice();
+    let lengths = inputArray.map(elem => elem.length);
+    let longest = Math.max(...lengths);
+    lengths.forEach((elem, idx) => {
+        if (elem === longest) {
+            resultsArray.push(inputArray[idx]);
+        }
+    });
+    return resultsArray;
+}
