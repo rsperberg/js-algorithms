55f2c6d44fd1e4e9bb3745025678f75b836a210d
Roger Sperberg <rsperberg@gmail.com>
2017-10-24 19:18:29 -0400

    Add alternate

--- arcade/12-sortByHeight.js
+++ arcade/12-sortByHeight.js
@@ -21,5 +21,25 @@ Sorted array a with all the trees untouched
 */
 
 function sortByHeight(a) {
+// in a copy of a, filter out the -1's then sort
+// loop through array a
+//   if -1 then push -1 to a new array
+//   if not, then shift element from copy and push to new array
+    let returnable = [];
+    let aCopy = a.filter(elem => elem > -1).sort((x, y) => x - y);
+    a.forEach((elem, idx) => {
+        if (elem === -1) {
+            returnable.push(-1);
+        } else {
+            returnable.push(aCopy.shift());
+        }
+    });
+    return returnable;
+}
 
+// use map to insert heights
+// dcrod
+function sortByHeight(a) {
+  var b = a.slice().sort((a, b) => a * b > 0 ? a - b : b - a);  // asc, -1's at end
+  return a.map(e => e != -1 ? b.shift : -1)
 }
